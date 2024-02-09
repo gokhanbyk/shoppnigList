5aa5dfa6deb1850bef88489e388793c07654270d
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-02-09 17:53:09 +0300

    filter items

--- script.js
+++ script.js
@@ -68,7 +68,18 @@ function clearItems() {
 }
 
 function filterItems(e) {
-  const text = e.target.value;
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
 
 function checkUI() {
